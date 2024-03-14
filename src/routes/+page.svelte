432fb36876f8a4ef743a31a32673c3ae0e503443
Adriano Weverton <adriwco@gmail.com>
2024-03-13 21:08:59 -0300

    header init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,44 @@
-<h1>Hello World</h1>
-<h2>Teste</h2>
-<p>texto <a href="/">link</a></p>
+<script>
+  import imgHome from '../assets/musculacao.jpg';
+</script>
+
+<section class="intro" style="background-image: url({imgHome})">
+  <div class="intro__wrapper wrap">
+    <h1>Você está pronto para o desafio?</h1>
+    <p>
+      Academia Local está pronta para te ajudar a chegar mais longe e alcançar
+      sua melhor performance.
+    </p>
+    <a class="btn" href="/sobre">Modalidades</a>
+  </div>
+</section>
 
 <style>
-  h1 {
-    color: var(--feat);
+  .intro {
+    min-height: 360px;
+    background-size: cover;
+    background-position: center;
+    display: grid;
+    align-content: center;
+    justify-content: start;
+    position: relative;
+  }
+  .intro::after {
+    content: '';
+    position: absolute;
+    inset: 0;
+    background-color: rgba(0, 0, 0, 0.55);
+  }
+  .intro__wrapper {
+    z-index: 1;
+  }
+  .intro__wrapper h1 {
+    max-width: 300px;
+  }
+  .intro__wrapper p {
+    max-width: 450px;
+  }
+  .btn {
+    margin-top: 24px;
   }
 </style>
